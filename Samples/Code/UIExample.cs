65c322a2f037eab383a18559dfb9538f931fa41c
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2022-01-18 21:41:28 +0100

    samples moved from UnityPlugins repo

---
+++ Samples/Code/UIExample.cs
@@ -0,0 +1,18 @@
+﻿using ProceduralLevel.UnityPlugins.Common.Unity.Extended;
+using ProceduralLevel.UnityPlugins.UI.Unity;
+using UnityEngine;
+
+namespace ProceduralLevel.UnityPlugins.UI.Example
+{
+	public class UIExample : ExtendedMonoBehaviour
+	{
+		[SerializeField]
+		private UIManager m_Manager = null;
+
+		private void Awake()
+		{
+			m_Manager.Initialize();
+			m_Manager.GetPanel<TestPanel>().Show();
+		}
+	}
+}
---
+++ Samples/Code/Unity/Panel/TestPanel.cs
@@ -0,0 +1,32 @@
+﻿using ProceduralLevel.Common.Event;
+using ProceduralLevel.UnityPlugins.UI.Unity;
+using UnityEngine;
+
+namespace ProceduralLevel.UnityPlugins.UI.Example
+{
+	public class TestPanel : APanel
+	{
+		protected override void OnPrepare(EventBinder binder)
+		{
+			IInteractiveComponent[] elements = GetComponentsInChildren<IInteractiveComponent>();
+			int length = elements.Length;
+			for(int x = 0; x < length; ++x)
+			{
+				Bind(binder, elements[x]);
+			}
+		}
+
+		public new void Show()
+		{
+			base.Show();
+		}
+
+		private void Bind(EventBinder binder, IInteractiveComponent element)
+		{
+			InteractionHandler handler = element.InteractionHandler;
+			binder.Bind(handler.OnActive, (active) => Debug.Log($"Active: {element.name}, {active}"));
+			binder.Bind(handler.OnHovered, (hovered) => Debug.Log($"Hovered: {element.name}, {hovered}"));
+			binder.Bind(handler.OnClick, () => Debug.Log($"Clicked: {element.name}"));
+		}
+	}
+}
