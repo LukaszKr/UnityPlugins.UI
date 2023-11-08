8694ab7aab1fde25b776b7978e95984c9f4e9c74
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2023-11-08 23:21:57 +0100

    update context components

--- Unity/Element/AUIContextElement.cs
+++ Unity/Element/AUIContextElement.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ProceduralLevel.Common.Event;
 
 namespace ProceduralLevel.UI.Unity
@@ -9,32 +10,64 @@ namespace ProceduralLevel.UI.Unity
 		protected TContext m_Context;
 		private readonly EventBinder m_ContextBinder = new EventBinder();
 
+		private bool m_Initialized;
+		private bool m_ContextIsSet;
+
+		public void ClearContext()
+		{
+			if(m_ContextIsSet)
+			{
+				Detach();
+			}
+		}
+
 		public void SetContext(TContext context)
 		{
-			TryInitialize();
-			if(context == m_Context)
+			if(!m_Initialized)
+			{
+				TryInitialize();
+				m_Initialized = true;
+			}
+
+			if(EqualityComparer<TContext>.Default.Equals(context, m_Context))
 			{
 				throw new InvalidOperationException();
 			}
 
+			if(m_ContextIsSet)
+			{
+				Replace(m_Context);
+			}
+			else
+			{
+				Attach(context);
+			}
+
+			m_ContextBinder.UnbindAll();
+		}
+
+		private void Attach(TContext context)
+		{
+			m_ContextIsSet = true;
+			m_ContextBinder.UnbindAll();
+			m_Context = context;
+			OnAttach(m_ContextBinder);
+		}
+
+		private void Replace(TContext context)
+		{
 			m_ContextBinder.UnbindAll();
 			TContext oldContext = m_Context;
 			m_Context = context;
-			if(context != null)
-			{
-				if(oldContext != null)
-				{
-					OnReplace(m_ContextBinder, oldContext);
-				}
-				else
-				{
-					OnAttach(m_ContextBinder);
-				}
-			}
-			else if(oldContext != null)
-			{
-				OnDetach();
-			}
+			OnReplace(m_ContextBinder, oldContext);
+		}
+
+		private void Detach()
+		{
+			m_ContextIsSet = false;
+			m_Context = default;
+			m_ContextBinder.UnbindAll();
+			OnDetach();
 		}
 
 		protected virtual void OnReplace(EventBinder binder, TContext oldContext)
--- Unity/Panel/AContextPanel.cs
+++ Unity/Panel/AContextPanel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ProceduralLevel.Common.Event;
 
 namespace ProceduralLevel.UI.Unity
@@ -8,6 +9,8 @@ namespace ProceduralLevel.UI.Unity
 	{
 		protected TContext m_Context;
 		private readonly EventBinder m_ContextBinder = new EventBinder();
+
+		private bool m_ContextIsSet;
 
 		protected override void Awake()
 		{
@@ -26,35 +29,59 @@ namespace ProceduralLevel.UI.Unity
 		protected override void OnHide()
 		{
 			base.OnHide();
-			SetContext(null);
+			ClearContext();
 		}
 
 		#region Context
+		public void ClearContext()
+		{
+			if(m_ContextIsSet)
+			{
+				Detach();
+			}
+		}
+
 		public void SetContext(TContext context)
 		{
-			if(context == m_Context)
+			if(EqualityComparer<TContext>.Default.Equals(context, m_Context))
 			{
 				throw new InvalidOperationException();
 			}
 
+			if(m_ContextIsSet)
+			{
+				Replace(m_Context);
+			}
+			else
+			{
+				Attach(context);
+			}
+
+			m_ContextBinder.UnbindAll();
+		}
+
+		private void Attach(TContext context)
+		{
+			m_ContextIsSet = true;
+			m_ContextBinder.UnbindAll();
+			m_Context = context;
+			OnAttach(m_ContextBinder);
+		}
+
+		private void Replace(TContext context)
+		{
 			m_ContextBinder.UnbindAll();
 			TContext oldContext = m_Context;
 			m_Context = context;
-			if(context != null)
-			{
-				if(oldContext != null)
-				{
-					OnReplace(m_ContextBinder, oldContext);
-				}
-				else
-				{
-					OnAttach(m_ContextBinder);
-				}
-			}
-			else if(oldContext != null)
-			{
-				OnDetach();
-			}
+			OnReplace(m_ContextBinder, oldContext);
+		}
+
+		private void Detach()
+		{
+			m_ContextIsSet = false;
+			m_Context = default;
+			m_ContextBinder.UnbindAll();
+			OnDetach();
 		}
 
 		protected virtual void OnReplace(EventBinder binder, TContext oldContext)
