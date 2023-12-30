6f4ca7142406411449640735b3db1555b0df6cc2
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2023-12-30 18:13:41 +0100

    add localization support to UIText class

--- Unity/Element/Basic/Text/UIText.cs
+++ Unity/Element/Basic/Text/UIText.cs
@@ -1,11 +1,15 @@
 ﻿using ProceduralLevel.Common.Event;
 using UnityEngine;
 using TMPro;
+using ProceduralLevel.Localization.Unity;
 
 namespace ProceduralLevel.UI.Unity
 {
 	public class UIText : AUIElement
 	{
+		private bool m_IsLocalized;
+		private LocalizationKey m_Key;
+
 		[SerializeField]
 		private TextMeshProUGUI m_Text = null;
 
@@ -18,6 +22,48 @@ namespace ProceduralLevel.UI.Unity
 		public void SetText(string text)
 		{
 			m_Text.text = text;
+			SetLocalized(false);
 		}
+
+		#region Localization
+		public void SetLocalization(LocalizationKey key)
+		{
+			m_Key = key;
+			SetLocalized(true);
+		}
+
+		private void RefreshLocalization()
+		{
+			if(m_IsLocalized)
+			{
+				m_Text.text = m_Key.GetTranslation();
+			}
+		}
+
+		private void SetLocalized(bool localized)
+		{
+			if(m_IsLocalized != localized)
+			{
+				m_IsLocalized = localized;
+				if(localized)
+				{
+					LocalizationManager.Instance.OnLanguageChanged.AddListener(OnLanguageChangedHandler);
+					RefreshLocalization();
+				}
+				else
+				{
+					LocalizationManager.Instance.OnLanguageChanged.RemoveListener(OnLanguageChangedHandler);
+					m_Key = default;
+				}
+			}
+		}
+		#endregion
+
+		#region Callbacks
+		private void OnLanguageChangedHandler(LanguageConfig config)
+		{
+			RefreshLocalization();
+		}
+		#endregion
 	}
 }
