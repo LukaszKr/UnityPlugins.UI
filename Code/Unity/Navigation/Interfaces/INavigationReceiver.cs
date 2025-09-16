13ac20e9dc1f0aeb3fe65b32a871f785e8693070
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-09-16 14:07:45 +0200

    remove recttransform from interface

--- Code/Unity/Navigation/Interfaces/INavigationReceiver.cs
+++ Code/Unity/Navigation/Interfaces/INavigationReceiver.cs
@@ -6,7 +6,6 @@ namespace UnityPlugins.UI.Unity
 	public interface INavigationReceiver
 	{
 		bool IsNavigationActive { get; }
-		RectTransform RectTransform { get; }
 
 		void NavigationSelected();
 		void NavigationDeselected();
