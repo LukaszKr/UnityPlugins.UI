317924764c734f3960e932cd7c99743b62b2635f
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-09-18 21:04:56 +0200

    remove unused

--- Code/Unity/Extensions/RectTransformExt.cs
+++ Code/Unity/Extensions/RectTransformExt.cs
@@ -48,18 +48,5 @@ namespace UnityPlugins.UI.Unity
 			rect.anchorMax = new Vector2(1f, 1f);
 			rect.sizeDelta = new Vector2(0f, 0f);
 		}
-
-		//public static void ApplyLayout(this RectTransform rect, Layout layout)
-		//{
-		//	rect.anchorMin = new Vector2(0f, 1f);
-		//	rect.anchorMax = new Vector2(0f, 1f);
-		//	Vector2 pivot = rect.pivot;
-		//	LayoutRect layoutRect = layout.Rect;
-		//	Vector2 position = new Vector2(layoutRect.X, -layoutRect.Y);
-		//	position.x += pivot.x*layoutRect.Width;
-		//	position.y -= pivot.y*layoutRect.Height;
-		//	rect.anchoredPosition = position;
-		//	rect.sizeDelta = new Vector2(layoutRect.Width, layoutRect.Height);
-		//}
 	}
 }
